254962e62ac3e15ca96ff70c00eed86020a83ec1
AlexanderA28 <58190710+AlexanderA28@users.noreply.github.com>
2021-03-16 19:36:15 +0000

    added js

--- show_number.js
+++ show_number.js
@@ -1,3 +1,6 @@
-var display_number =  document.getElementById('number').value
 
-document.getElementById("myBtn").onclick = display_number;
+function myFunction() {
+    var display_number =  document.getElementById('number').value
+
+    document.getElementById("myBtn").onclick = window.alert(display_number);
+}
